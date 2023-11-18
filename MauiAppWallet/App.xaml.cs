9b061676ed585e406b34d77a58871409982a8d0f
Diego Aquino <diegol.aquino@gmail.com>
2023-11-18 11:12:29 -0300

    Add project files.

---
+++ MauiAppWallet/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppWallet
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiAppWallet/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppWallet
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
